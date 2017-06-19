c79c3581f3e9b5e2d57527ee7e09c8b3c52d5708
thomai-sta <thomai.stathopoulou@gmail.com>
2017-06-19 19:31:18 +0300

    unfinished

---
+++ New Year Present/main.cpp
@@ -0,0 +1,37 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> l(n);
+  for (int l_i = 0; l_i < n; l_i++)
+  {
+    cin >> l[l_i];
+  }
+  return 0;
+}
